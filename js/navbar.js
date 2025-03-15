b11ee4d17959ed0358e28a10b8c9fa4b00db61a9
AdanShah08 <shahadan496@gmail.com>
2025-03-15 12:57:33 +0500

    ydc

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.getElementById("spinner").classList.add("hide");
+    }, 1000); // Hide after 1 second
+});
